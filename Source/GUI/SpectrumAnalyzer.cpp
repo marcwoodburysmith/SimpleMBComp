ed8189049d620de8f5cf4f52a6bd7623558b178d
marcwoodburysmith <marc.woodbury-smith@newcastle.ac.uk>
2024-05-31 08:54:11 +0100

    64. Fixed text collisions

--- Source/GUI/SpectrumAnalyzer.cpp
+++ Source/GUI/SpectrumAnalyzer.cpp
@@ -85,21 +85,21 @@ void SpectrumAnalyzer::paint (juce::Graphics& g)
 //    g.setColour(Colours::white);
 //    g.strokePath(responseCurve, PathStrokeType(2.f));
     
-    Path border;
-    
-    border.setUsingNonZeroWinding(false);
-    
-    border.addRoundedRectangle(getRenderArea(bounds), 4);
-    border.addRectangle(getLocalBounds());
-    
-    g.setColour(Colours::black);
+//    Path border;
+//    
+//    border.setUsingNonZeroWinding(false);
+//    
+//    border.addRoundedRectangle(getRenderArea(bounds), 4);
+//    border.addRectangle(getLocalBounds());
+//    
+//    g.setColour(Colours::black);
     
 //    g.fillPath(border);
     
     drawTextLabels(g, bounds);
     
-    g.setColour(Colours::orange);
-    g.drawRoundedRectangle(getRenderArea(bounds).toFloat(), 4.f, 1.f);
+//    g.setColour(Colours::orange);
+//    g.drawRoundedRectangle(getRenderArea(bounds).toFloat(), 4.f, 1.f);
 }
 
 std::vector<float> SpectrumAnalyzer::getFrequencies()
@@ -205,7 +205,8 @@ void SpectrumAnalyzer::drawTextLabels(juce::Graphics &g, juce::Rectangle<int> bo
 
         r.setSize(textWidth, fontHeight);
         r.setCentre(x, 0);
-        r.setY(1);
+//        r.setY(1);
+        r.setY(bounds.getY());
         
         g.drawFittedText(str, r, juce::Justification::centred, 1);
     }
@@ -225,7 +226,8 @@ void SpectrumAnalyzer::drawTextLabels(juce::Graphics &g, juce::Rectangle<int> bo
         
         Rectangle<int> r;
         r.setSize(textWidth, fontHeight);
-        r.setX(getWidth() - textWidth);
+//        r.setX(getWidth() - textWidth);
+        r.setX(bounds.getRight() - textWidth);
         r.setCentre(r.getCentreX(), y);
         
         g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::lightgrey );
@@ -235,7 +237,8 @@ void SpectrumAnalyzer::drawTextLabels(juce::Graphics &g, juce::Rectangle<int> bo
         str.clear();
         str << (gDb - 24.f);
 
-        r.setX(1);
+//        r.setX(1);
+        r.setX(bounds.getX() + 1);
         textWidth = g.getCurrentFont().getStringWidth(str);
         r.setSize(textWidth, fontHeight);
         g.setColour(Colours::lightgrey);
